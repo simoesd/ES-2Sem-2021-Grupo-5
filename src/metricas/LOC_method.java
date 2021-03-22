f6470b94fea4d2a0204132ecc0b45951e0bf1eaf
dianamml17 <dimariaml12@gmail.com>
2021-03-22 17:54:44 +0000

    LOC_method completed

--- src/metricas/LOC_method.java
+++ src/metricas/LOC_method.java
@@ -3,10 +3,13 @@ package metricas;
 import java.io.File;
 import java.util.ArrayList;
 
-public class LOC_method extends Metrica{
-	
-	private final String filter = "class";
-	
+import com.codahale.metrics.Counter;
+
+public class LOC_method extends Metrica {
+
+	// private final String filter = "class";
+	private Counter methodName = new Counter();
+
 	public LOC_method(Maestro metricas) {
 		super(metricas);
 	}
@@ -15,22 +18,27 @@ public class LOC_method extends Metrica{
 	protected void extractMetrics() {
 		ArrayList<File> filesInDirectory = getMetricas().getFilesInDirectory();
 		for (File file : filesInDirectory) {
-			this.openReadFile(file);
 			String absolutePath = file.getAbsolutePath();
 			setPackageClassName(getMetricas().cutAbsolutePath(absolutePath));
+			methodName = new Counter();
+			this.openReadFile(file);
 		}
 	}
 
 	@Override
 	protected void applyFilter(String s) {
 		String[] line = s.split(" ");
-		String[] filterToApply = filter.split(",");
-		for (String l : line) {
-			for (String f : filterToApply) {
-				if (l.equals(f)) {
-					// faz isto
-				}
-			}
+		// String[] filterToApply = filter.split(",");
+		String temp = methodName(s, line);
+
+		if (!temp.isBlank()) {
+			methodName = new Counter();
+			methodName = this.counter(getPackageClassName() + "." + temp);
+			methodName.inc();
+		} else {
+			if (!s.isBlank() && !s.contains("@Override"))
+				methodName.inc();
+
 		}
 	}
 }
