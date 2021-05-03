1a1e400f401c9d7cf1bed8e6af682f381ee9bbf0
simoesd <davefernans@gmail.com>
2021-05-03 20:22:44 +0100

    Writing and reading from rule history file changed
    now reads and writes all the entries at once to avoid outputstream header conflicts

--- src/rules/RuleFileManager.java
+++ src/rules/RuleFileManager.java
@@ -1,6 +1,5 @@
 package rules;
 
-import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -8,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,17 +20,20 @@ public class RuleFileManager {
     public static void writeEntry(List<Rule> rules)
     {
         File ruleFile = new File(HISTORY_FILE_PATH);
+        HashMap<String, List<Rule>> readInfo = new HashMap<>();
         try
         {
+            readInfo = readRules();
             FileOutputStream fileWriter = new FileOutputStream(ruleFile);
-            ruleFile.createNewFile();
             ObjectOutputStream oos = new ObjectOutputStream(fileWriter);
 
             Date date = new Date(System.currentTimeMillis());
             DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
             
-            RuleHistoryEntry newEntry = new RuleHistoryEntry(dateFormat.format(date), rules);
-            oos.writeObject(newEntry);
+            readInfo.put(dateFormat.format(date), rules);
+            oos.writeObject(readInfo);
+            oos.close();
+            fileWriter.close();
         } catch (IOException e) {
             
         } finally {
@@ -49,16 +50,10 @@ public class RuleFileManager {
             ruleFile.createNewFile();
             fileReader = new FileInputStream(ruleFile);
             ObjectInputStream ois = new ObjectInputStream(fileReader);
-            for (;;)
-            {
-                RuleHistoryEntry entry = (RuleHistoryEntry)ois.readObject();
-                readInfo.put(entry.timeStamp, entry.rules);
-            }
-            
-        } catch (EOFException e1) {
+            readInfo = (HashMap<String, List<Rule>>)ois.readObject();
+            ois.close();
             fileReader.close();
-        }
-        catch (IOException | ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         } finally {
             return readInfo;
@@ -73,18 +68,6 @@ public class RuleFileManager {
     
 }
 
-
-class RuleHistoryEntry implements Serializable{
-    String timeStamp;
-    List<Rule> rules;
-    
-
-    RuleHistoryEntry(String timeStamp, List<Rule> rules)
-    {
-        this.timeStamp = timeStamp;
-        this.rules = rules;
-    }
-}
 
 
 
