90b20de4a14e78d66f600bffb4c41b3f9acaa3b5
dcandeias20 <dfbcs@iscte-iul.pt>
2021-05-06 17:22:33 +0100

    MetricRegistryToTest

---
+++ src/metricas/MetricRegistryToTest.java
@@ -0,0 +1,60 @@
+package metricas;
+
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+
+public class MetricRegistryToTest extends MetricRegistry {
+
+	private ConcurrentMap<String, Counter> hello = new ConcurrentHashMap<String, Counter>();
+
+	@Override
+	public SortedMap<String, Counter> getCounters() {
+		// TODO Auto-generated method stub
+		return getCounters(MetricFilter.ALL);
+	}
+
+	@Override
+	public SortedMap<String, Counter> getCounters(MetricFilter filter) {
+		return getMetrics(Counter.class, filter);
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T extends Metric> SortedMap<String, T> getMetrics(Class<T> klass, MetricFilter filter) {
+		final TreeMap<String, T> timers = new TreeMap<>();
+		for (Map.Entry<String, Counter> entry : hello.entrySet()) {
+			if (klass.isInstance(entry.getValue()) && filter.matches(entry.getKey(), entry.getValue())) {
+				timers.put(entry.getKey(), (T) entry.getValue());
+			}
+		}
+		return timers;
+	}
+
+	public ConcurrentMap<String, Counter> buildMap(SortedMap<String, Counter> sm) {
+		ConcurrentMap<String, Counter> teste = new ConcurrentHashMap<String, Counter>(sm);
+		hello = teste;
+		return teste;
+	}
+
+	private MetricRegistry setCounters(SortedMap<String, Counter> sm) {
+		this.buildMap(sm);
+		return this;
+	}
+
+	public MetricRegistry addCounter(String key, Counter value) {
+		SortedMap<String, Counter> newMr = this.getCounters();
+
+		newMr.put(key, value);
+		setCounters(newMr);
+
+		return this;
+
+	}
+}
