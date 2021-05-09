81408e54d50549f192b8963a91c53dbd7fc7a859
dianamml17 <dimariaml12@gmail.com>
2021-05-09 13:43:13 +0100

    Javadocs das metricas completadas

--- src/metricas/CYCLO_method.java
+++ src/metricas/CYCLO_method.java
@@ -1,29 +1,61 @@
 package metricas;
 
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
+/** 
+ * CYCLO_method is a type of {@code Metrica} that increments its counter everytime
+ * it finds certain words in the code inside of a method, evaluating the cyclomatic complexity of the method
+ * @see Metrica
+ * @see Maestro
+ * @since 1.0
+ */
+
 public class CYCLO_method extends Metrica {
 
+	/** 
+	 * String representing the words (separated by commas) that increment the counter of the {@code CYCLO_method}  
+	 */
 	private final String filter = "for,if,while,case";
+	
+	/** 
+	 * Constructs and initializes a {@code CYCLO_method}
+	 */
 
 	public CYCLO_method() {
 		metricName = "CYCLO_METHOD";
 		isClassMetric = false;
 	}
+	
+	/** 
+	 * Constructs and initializes a {@code CYCLO_method}, given an object {@code Maestro} and a String. 
+	 * This constructor is used only for Unit Testing
+	 * @param maestro the maestro that will manipulate all the metrics
+	 * @param unitTest the string used for unit testing
+	 */
+	
 	public CYCLO_method(Maestro maestro, String unitTest) { //Unit test
 		super(maestro, unitTest);
 		metricName = "CYCLO_METHOD";
 		isClassMetric = false;
 	}
+	
+	/** 
+	 * Constructs and initializes a {@code CYCLO_method}, given an object {@code Maestro}.
+	 * @param maestro the maestro that will manipulate all the metrics
+	 */
+	
 	public CYCLO_method(Maestro maestro) {
 		super(maestro);
 		metricName = "CYCLO_METHOD";
 		isClassMetric = false;
 	}
 
+	/**
+     * Prepares the names of the files that will be evaluated and calls filterCode() for each of the files
+     */
 	@Override
 	public void extractMetrics() {		
 		ArrayList<File> filesInDirectory = getMaestro().getFilesInDirectory();
@@ -34,6 +66,11 @@ public class CYCLO_method extends Metrica {
 		}
 	}
 	
+	/**
+     * Analyzes the given methodCode, incrementing the counter if it encounters any of the 
+     * words present in the filter 
+     * @param methodCode string representing the line of the method that will be analyzed 
+     */
 	
 	@Override
 	public void applyMetricFilter(String methodCode) {  
--- src/metricas/WMC_class.java
+++ src/metricas/WMC_class.java
@@ -6,26 +6,58 @@ import java.util.SortedMap;
 
 import com.codahale.metrics.Counter;
 
+/** 
+ * WMC_class is a type of {@code Metrica} that sums the cyclomatic complexity of each method it contains,
+ * evaluating the cyclomatic complexity of the class
+ * @see Metrica
+ * @see Maestro
+ * @since 1.0
+ */
+
 public class WMC_class extends Metrica {
 	
+	/** 
+	 * SortedMap representing the CYCLO_Method counters
+	 */
 	private SortedMap<String, Counter> cycloSortedMap;
 
+	/** 
+	 * Constructs and initializes a {@code WMC_class}
+	 */
 	public WMC_class() {
 		metricName = "WMC_CLASS";
 		isClassMetric = true;
 	}
+	
+	/** 
+	 * Constructs and initializes a {@code WMC_class}, given an object {@code Maestro} and a String. 
+	 * This constructor is used only for Unit Testing
+	 * @param maestro the maestro that will manipulate all the metrics
+	 * @param unitTest the string used for unit testing
+	 */
+	
 	public WMC_class (Maestro maestro, String unitTest) {
 		super(maestro, unitTest);
 		metricName = "LOC_CLASS";
 		isClassMetric = true;
 		
 	}
+
+	/** 
+	 * Constructs and initializes a {@code WMC_class}, given an object {@code Maestro}.
+	 * @param maestro the maestro that will manipulate all the metrics
+	 */
 	
 	public WMC_class(Maestro maestro) {
 		super(maestro);	
 		metricName = "WMC_CLASS";
 		isClassMetric = true;
 	}
+	
+	/**
+     * Prepares the names of the files that will be evaluated and calculates the cyclomatic complexity of the class
+     * by using the cycloSortedMap
+     */
 
 	@Override
 	public void extractMetrics() {
@@ -42,7 +74,6 @@ public class WMC_class extends Metrica {
 				}
 			}
 		}
-		
 	}
 
 	@Override
